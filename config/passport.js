b95cc0ea473ff06aae1f5f56b317258cf2d423b2
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-26 09:44:04 +0300

    Code local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,34 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const prisma = require("../db/prisma");
+const bcrypt = require("bcryptjs");
+
+const verfyCallback = async (username, password, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return done(null, false, {
+        message: "Incorrect username",
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return done(null, false, {
+        message: "Incorrect password",
+      });
+    }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+};
+
+passport.use(new LocalStrategy(verfyCallback));
