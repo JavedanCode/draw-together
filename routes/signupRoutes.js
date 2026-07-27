e04e4ca3d70ffb0df425720d4e1df4be2aadd15b
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-27 13:04:42 +0300

    Add signup validator to signup post route

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const signupValidation = require("../validation/signupValidation");
 const router = express.Router();
 
 router.get("/signup", signUpController.getSignupForm);
-router.post("/signup", signUpController.createUser);
+router.post("/signup", signupValidation, signUpController.createUser);
 
 module.exports = router;
