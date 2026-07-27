f7779f5a15cdad6ab335cec27eff9aa69eb0222d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-27 13:05:48 +0300

    Add signup controller

---
+++ controllers/signupController.js
@@ -0,0 +1,32 @@
+const signupQueries = require("../db/queries/signupQueries");
+const { validationResult } = require("express-validator");
+
+const getSignupForm = (req, res) => {
+  res.render("signup", { title: "Signup" });
+};
+
+const createUser = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).render("signup", {
+      title: "Signup",
+      errors: errors.array(),
+      oldInput: req.body,
+    });
+  }
+  const { email, username, password } = req.body;
+
+  const user = {
+    email,
+    username,
+    password,
+  };
+
+  try {
+    await signupQueries.createUserQuery(user);
+    res.redirect("/login");
+  } catch (err) {
+    next(err);
+  }
+};
