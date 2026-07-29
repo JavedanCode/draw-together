f9f9300e90c2d7e6f4ab13e46f968c841aeff847
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-29 09:49:12 +0300

    Add controller for membership routes

---
+++ controllers/membershipController.js
@@ -0,0 +1,30 @@
+const membershipQueries = require("../db/queries/membershipQueries");
+
+const getMembershipForm = (req, res) => {
+  res.render("membership", { title: "Become a member" });
+};
+
+const makeUserMember = async (req, res, next) => {
+  try {
+    if (req.user?.featuredMember) {
+      const err = new Error("You're already a member silly!");
+      err.status = 409;
+      throw err;
+    }
+
+    if (process.env.MEMBER_CODE !== req.body.memberCode) {
+      const err = new Error("The code is wrong!!!");
+      err.status = 409;
+      throw err;
+    }
+    await membershipQueries.updateMembershipQuery(req.user.id);
+    return res.redirect("/test");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getMembershipForm,
+  makeUserMember,
+};
