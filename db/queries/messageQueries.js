c8a8007e0be9fbca4517f3492f8ff222f4804cd7
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-29 11:28:00 +0300

    Create message query functions

---
+++ db/queries/messageQueries.js
@@ -0,0 +1,62 @@
+const prisma = require("../prisma");
+
+const addMessageQuery = async (imageData, userId) => {
+  return prisma.drawing.create({
+    data: {
+      imageData,
+      authorId: userId,
+    },
+  });
+};
+
+const deleteMessageQuery = async (messageId) => {
+  await prisma.drawing.delete({
+    where: {
+      id: Number(messageId),
+    },
+  });
+};
+
+const updateMessageQuery = async (id, data) => {
+  await prisma.drawing.update({
+    where: {
+      id: Number(id),
+    },
+    data,
+  });
+};
+
+const getUserMessagesQuery = async (userId) => {
+  return prisma.drawing.findMany({
+    where: { authorId: Number(userId) },
+  });
+};
+
+const getAllMessagesQuery = async () => {
+  return prisma.drawing.findMany({
+    include: {
+      author: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+const getMessageQuery = async (userId, drawingId) => {
+  return prisma.drawing.findFirst({
+    where: {
+      id: Number(drawingId),
+      authorId: Number(userId),
+    },
+  });
+};
+
+module.exports = {
+  addMessageQuery,
+  deleteMessageQuery,
+  updateMessageQuery,
+  getAllMessagesQuery,
+  getUserMessagesQuery,
+  getMessageQuery,
+};
