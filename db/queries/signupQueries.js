56482c47ba5574528286153cd02891284262df22
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-30 10:27:07 +0300

    Remove redundant duplicate checks in query

--- db/queries/signupQueries.js
+++ db/queries/signupQueries.js
@@ -2,33 +2,9 @@ const prisma = require("../prisma");
 const bcrypt = require("bcryptjs");
 
 const createUserQuery = async (user) => {
-  const userExists = await prisma.user.findUnique({
-    where: {
-      username: user.username,
-    },
-  });
-
-  if (userExists) {
-    const err = new Error("Username already exists");
-    err.status = 409;
-    throw err;
-  }
-
-  const emailExists = await prisma.user.findUnique({
-    where: {
-      email: user.email,
-    },
-  });
-
-  if (emailExists) {
-    const err = new Error("User with this email already exists");
-    err.status = 409;
-    throw err;
-  }
-
   const hashedPassword = await bcrypt.hash(user.password, 10);
 
-  await prisma.user.create({
+  return prisma.user.create({
     data: {
       email: user.email,
       username: user.username,
