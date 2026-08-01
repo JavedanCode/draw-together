9c23aea5150ace5c63a75ae2cea5d054001141b7
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-01 14:33:13 +0300

    Separate update routes for email, username and password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,17 +3,38 @@ const router = express.Router();
 
 const ensureAuthentication = require("../controllers/middleware/ensureAuthentication");
 const userController = require("../controllers/userController");
-const updateUserValidation = require("../validation/updateUserValidation");
+const updateUsernameValidation = require("../validation/updateUsernameValidation");
+const updateEmailValidation = require("../validation/updateEmailValidation");
+const updatePasswordValidation = require("../validation/updatePasswordValidation");
+const handleValidationErrors = require("../controllers/middleware/handleValidationErrors");
 
 router.get("/", ensureAuthentication, userController.getUserInfo);
 
-router.get("/update", ensureAuthentication, userController.getUpdateUserForm);
+//Removed get user update form
+//Separated routes for updating user details.
 
 router.post(
-  "/update",
+  "/update-username",
   ensureAuthentication,
-  updateUserValidation,
-  userController.updateUserInfo,
+  updateUsernameValidation,
+  handleValidationErrors("profile", "Account Information", "username"),
+  userController.updateUsername,
+);
+
+router.post(
+  "/update-email",
+  ensureAuthentication,
+  updateEmailValidation,
+  handleValidationErrors("profile", "Account Information", "email"),
+  userController.updateEmail,
+);
+
+router.post(
+  "/update-password",
+  ensureAuthentication,
+  updatePasswordValidation,
+  handleValidationErrors("profile", "Account Information", "password"),
+  userController.updatePassword,
 );
 
 router.post("/delete", ensureAuthentication, userController.deleteUser);
